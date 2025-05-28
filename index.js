fdb291eba769ce69fbe26d5063cba7f9b27b96ad
Ville Sirviö <VSirvio@users.noreply.github.com>
2025-05-28 12:41:32 +0300

    Update version number v3 -> v4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1> v3')
+  res.send('<h1>Hello World!</h1> v4')
 })
 
 const PORT = 8080
